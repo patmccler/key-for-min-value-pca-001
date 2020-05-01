c991c51739766719882b666e8c5267c797499ee8
Pat McClernan <patmccler@gmail.com>
2020-05-01 08:45:14 -0400

    solved with inject

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
+require "pry"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  # saves the smaller value in memo, and takes the first elem of memo at the end (the key)
+  smallest = name_hash.inject do|memo, (k, v)|
+    memo = v < memo[1] ? [k,v] : memo
+  end
+  #handle the empty hash case...
+  smallest ? smallest[0] : nil
 end
